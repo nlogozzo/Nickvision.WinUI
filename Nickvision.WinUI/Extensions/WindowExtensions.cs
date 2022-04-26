481c5ad0db52d244f1304ca4fafe8e54fef39887
Nick <nlogozzo225@gmail.com>
2022-04-25 23:06:30 -0400

    V2022.4.4
    - Added ApplyDarkTitleBar method to WindowExtensions

--- Nickvision.WinUI/Extensions/WindowExtensions.cs
+++ Nickvision.WinUI/Extensions/WindowExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
-using PInvoke;
+using System;
+using Vanara.PInvoke;
 using WinRT.Interop;
 
 namespace Nickvision.WinUI.Extensions;
@@ -13,5 +14,15 @@ public static class WindowExtensions
     /// Maximizes a window.
     /// </summary>
     /// <param name="window">The window to maximize</param>
-    public static void Maximize(this Window window) => User32.ShowWindow(WindowNative.GetWindowHandle(window), User32.WindowShowStyle.SW_SHOWMAXIMIZED);
+    public static void Maximize(this Window window) => User32.ShowWindow(WindowNative.GetWindowHandle(window), ShowWindowCommand.SW_SHOWMAXIMIZED);
+
+    /// <summary>
+    /// Changes the window's title bar to a dark theme.
+    /// </summary>
+    /// <param name="window">The window to change theme</param>
+    public static void ApplyDarkTitleBar(this Window window)
+    {
+        DwmApi.DwmSetWindowAttribute(WindowNative.GetWindowHandle(window), (DwmApi.DWMWINDOWATTRIBUTE)20, new IntPtr(1), sizeof(int));
+        UxTheme.SetWindowTheme(WindowNative.GetWindowHandle(window), "DarkMode_Explorer", null);
+    }
 }
